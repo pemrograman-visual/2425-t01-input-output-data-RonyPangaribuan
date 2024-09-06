3dd82e40f6d03a40c680fedd0caacc32ecf67b54
rony <ronypangaribuan2505@gmail.com>
2024-09-06 11:40:31 +0700

    Telah Menyelesaikan T01

--- T01.java
+++ T01.java
@@ -1,1 +1,36 @@
-// NIM - Your Name
+// 12S24040 -Rony Reynaldy Pangaribuan
+// 12S24023 -Jaya Bestina Simbolon
+
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String judul;
+        String penulis;
+        int tahunTerbit;
+        String penerbit;
+        String formatBukuElektronik;
+        double hargaPembelian;
+        double minimumMargin;
+        int stok;
+        double rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        formatBukuElektronik = input.nextLine();
+        hargaPembelian = Double.parseDouble(input.nextLine());
+        minimumMargin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBukuElektronik + "|" + hargaPembelian + "|" + minimumMargin + "|" + stok + "|" + rating);
+    }
+}
+
